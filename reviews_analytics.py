5f7097b70e17676bea319c3935e630f41e6aa3ab
Larry La <lailarry1226@gmail.com>
2024-10-14 14:15:27 +0800

    add count

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,10 +1,14 @@
 data = []
 count = 0
+a = 0
 with open('test.txt', 'r') as f:
     for line in f:
+        a = len(line) + a
         data.append(line)
         count += 1
-        if count % 2 == 0:
+        if count % 5 == 0:
             print(len(data))
 print('第一筆為', data[0])
 print('總共資料為', len(data), '筆')
+print('總共用了', a, '字元')
+print('平均data長度為', a / len(data))
